c8aff8bc154fde444a3a7ce2f1f116537f8f80c4
Nguyễn Văn Trọng <trongitnlu@gmail.com>
2020-06-03 18:01:59 +0700

    first commit

---
+++ windows/chatSocket/App.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+
+
+namespace winrt::chatSocket::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::chatSocket::implementation
+
+
